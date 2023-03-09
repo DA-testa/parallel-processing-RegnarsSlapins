3883bd5219577624caa7f51e3fc977c73244beb0
RegnarsSlapins <regnars.slapins@edu.rtu.lv>
2023-03-09 22:06:07 +0200

    V1

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ def parallel_processing(n, m, data):
     output = []
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
-
+    endOfProcess=[]
+    for i in range(n):
+        endOfProcess.append(data[0])
+        output.append([i,0])
+        del data[0]
+    while len(data)!=0:
+        # Thread which is about to end its process
+        index = endOfProcess.index(min(endOfProcess))
+        nextJobTime=data[0]
+        del data[0]
+        output.append([index, min(endOfProcess)])
+        endOfProcess[index]=endOfProcess[index]+nextJobTime
+    print(output)
     return output
 
 def main():
@@ -13,16 +25,28 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
+    entry = input()
+    if entry[0]=="I":
+        line1=input()
+        n = int(line1[0])
+        m = int(line1[-1])
+        data = list(map(int, input().split()))
+        if len(data)!=m:
+            print("Job count doesn't match")
+            exit()
+        if n>m:
+            print("More threads than jobs! Excess ignored.")
+            n=m
+        result = parallel_processing(n, m, data)
+        for thread, task in result:
+            print(thread, task)
+    else:
+        print("Incorrect entry")
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
 
     # TODO: create the function
-    result = parallel_processing(n,m,data)
-    
     # TODO: print out the results, each pair in it's own line
 
 
